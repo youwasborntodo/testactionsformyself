9bb572ba6706a52269f2e10ae27814cc2cd7799f
youwasborntodo <505072907@qq.com>
2021-12-03 13:56:39 +0800

    修改构建目录

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@
  * @Autor: Brian
  * @Date: 2021-12-02 17:11:23
  * @LastEditors: Brian
- * @LastEditTime: 2021-12-03 13:49:57
+ * @LastEditTime: 2021-12-03 13:56:02
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -12,9 +12,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+      base: '/testactionsformyself/',
       build: {
-        outDir: 'build',
-        assetsDir: '/testactionsformyself/assets'
+        outDir: 'build'
       },
       plugins: [vue()]
     }
