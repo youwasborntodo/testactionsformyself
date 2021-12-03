45c7d6fc277aeacf01bb078e019aad8e2e3459ec
youwasborntodo <505072907@qq.com>
2021-12-03 10:34:53 +0800

    修改构建路径

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-/*
- * @Description: 
- * @Version: 2.0
- * @Autor: Brian
- * @Date: 2021-12-02 17:11:23
- * @LastEditors: Brian
- * @LastEditTime: 2021-12-03 09:56:51
- */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  outDir: 'deploy',
-  plugins: [vue()]
+export default defineConfig(() => {
+    return {
+      build: {
+        outDir: 'build'
+      },
+      plugins: [vue()]
+    }
 })
