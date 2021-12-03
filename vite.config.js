c8f022be74496bc33d50db9e5c18fc59492bace2
youwasborntodo <505072907@qq.com>
2021-12-03 09:58:06 +0800

    修改构建目录名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Brian
+ * @Date: 2021-12-02 17:11:23
+ * @LastEditors: Brian
+ * @LastEditTime: 2021-12-03 09:56:51
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  outDir: 'deploy',
   plugins: [vue()]
 })
