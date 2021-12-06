21090623b5b06177688517d7cbca6abc1ae5689e
youwasborntodo <505072907@qq.com>
2021-12-06 17:31:37 +0800

    test vite build config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@
  * @Autor: Brian
  * @Date: 2021-12-02 17:11:23
  * @LastEditors: Brian
- * @LastEditTime: 2021-12-03 13:56:02
+ * @LastEditTime: 2021-12-06 17:31:13
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -14,7 +14,13 @@ export default defineConfig(() => {
     return {
       base: '/testactionsformyself/',
       build: {
-        outDir: 'build'
+        outDir: 'build',
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true
+          }
+        }
       },
       plugins: [vue()]
     }
